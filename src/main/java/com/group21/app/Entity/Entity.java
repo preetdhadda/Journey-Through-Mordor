66897588afc7ac64e23037f1bfd33b292a436c82
jsc48 <jaspartap_chahal@sfu.ca>
2022-11-01 14:45:33 -0700

    Remove comments

--- src/main/java/com/group21/app/Entity/Entity.java
+++ src/main/java/com/group21/app/Entity/Entity.java
@@ -3,7 +3,6 @@ package com.group21.app.Entity;
 import java.awt.*;
 
 public class Entity {
-    //public int xPos, yPos;
     public Point position;
     public String direction;
     public boolean collisionOn = false;

5f665315bed2b167e1d7d31810baf4f0e7ce3124
jsc48 <jaspartap_chahal@sfu.ca>
2022-11-01 14:51:26 -0700

    Remove unused imports/call background music

--- src/main/java/com/group21/app/Main.java
+++ src/main/java/com/group21/app/Main.java
@@ -1,21 +1,18 @@
 package com.group21.app;
-
-import javax.swing.*;
-
-import com.group21.app.Screen.MenuPanel;
-import com.group21.app.Screen.ScreenPanel;
-import com.group21.app.Screen.TitlePanel;
 import com.group21.app.Screen.UI;
+import com.group21.app.Entity.Music;
 
-import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
         UI ui = UI.getInstance();
         ui.makeTitleWindow();
+        Music.Music("src/main/resources/music/airtone_-_brokencloud_1.wav");
     }
 }
+
+// Music brokencloud by airtone (c) copyright 2022 Licensed under a Creative Commons Attribution Noncommercial  (3.0) license. http://dig.ccmixter.org/files/airtone/65484 
 
 // COMPILE THE CODE: mvn package
 // RUNNING THE CODE: java -cp target/Phase-2-1.0.jar com.group21.app.Main

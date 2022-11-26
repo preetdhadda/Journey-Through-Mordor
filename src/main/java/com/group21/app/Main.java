b0fefe49905b180771c41a6fea41a1b01c4ea1ec
Jimmy <jimmyhui92012@gmail.com>
2022-11-25 19:34:33 -0800

    Cleaned up main

--- src/main/java/com/group21/app/Main.java
+++ src/main/java/com/group21/app/Main.java
@@ -22,11 +22,8 @@ public class Main {
                 ui.makeTitleWindow();
             }
         });
-        //Music.setMusic("src/main/resources/music/airtone_-_brokencloud_1.wav");
+        Music.setMusic("src/main/resources/music/airtone_-_brokencloud_1.wav");
     }
 }
 
 // Music brokencloud by airtone (c) copyright 2022 Licensed under a Creative Commons Attribution Noncommercial  (3.0) license. http://dig.ccmixter.org/files/airtone/65484 
-
-// COMPILE THE CODE: mvn package
-// RUNNING THE CODE: java -cp target/Phase-2-1.0.jar com.group21.app.Main
